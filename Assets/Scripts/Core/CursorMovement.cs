8f999757f050e0337e993296532550252b61f861
Fabian Friedrich <friedrich.fabian@gmx.net>
2023-07-08 00:25:44 +0200

    Added kinda broken Cursor Path Trace

--- Assets/Scripts/Core/CursorMovement.cs
+++ Assets/Scripts/Core/CursorMovement.cs
@@ -7,6 +7,8 @@
 
 using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -14,36 +16,52 @@ namespace F4B1.Core
 {
     public class CursorMovement : MonoBehaviour
     {
-        [SerializeField] private LeanTweenType moveTweenType;
         [SerializeField] private float speed;
-
+        [SerializeField] private LineRenderer lineRenderer;
+        [SerializeField] private int renderPoints;
+        
         [SerializeField] private Vector2 bottomLeft;
         [SerializeField] private Vector2 topRight;
 
+        private LTBezierPath oldPath;
         private LTBezierPath path;
+        private LTBezierPath newPath;
         
         private void Awake()
         {
+            var endPos = new Vector2(Random.Range(bottomLeft.x, topRight.x), Random.Range(bottomLeft.y, topRight.y));
+            var position = transform.position;
+            
+            oldPath = CreateBezierPath(position, endPos);
+            path = oldPath;
+            newPath = path;
             StartCoroutine(nameof(MoveToRandomPosition));
         }
-        
+
+        private void Update()
+        {
+            UpdateLineRendererPath();
+        }
+
         private IEnumerator MoveToRandomPosition()
         {
             var endPos = new Vector2(Random.Range(bottomLeft.x, topRight.x), Random.Range(bottomLeft.y, topRight.y));
-            var position = transform.position;
-            path = CreateBezierPath(position, endPos);
+            
+            path = newPath;
+            newPath = CreateBezierPath(path.pts[3], endPos);
+            
             var distance = path.length;
-
             LeanTween.move(gameObject, path, distance / speed);
             yield return new WaitForSeconds(distance / speed);
 
+            oldPath = path;
             StartCoroutine(nameof(MoveToRandomPosition));
         }
 
         private LTBezierPath CreateBezierPath(Vector3 startPos, Vector3 endPos)
         {
-            var offsetStart = RandomVector(20);
-            var offsetEnd = RandomVector(20);
+            var offsetStart = RandomVector(3);
+            var offsetEnd = RandomVector(3);
 
             var pts = new[]
             {
@@ -74,6 +92,56 @@ namespace F4B1.Core
             Gizmos.DrawLine(bottomLeft, new Vector2(topRight.x, bottomLeft.y));
             Gizmos.DrawLine(topRight, new Vector2(topRight.x, bottomLeft.y));
             Gizmos.DrawLine(topRight, new Vector2(bottomLeft.x, topRight.y));
+        }
+
+        private void UpdateLineRendererPath()
+        {
+            var oldPathPoints = CalculateBezierCurveLine(oldPath, 200);
+            var pathPoints = CalculateBezierCurveLine(path, 200);
+            var newPathPoints = CalculateBezierCurveLine(newPath, 200);
+
+            var concat = oldPathPoints.Concat(pathPoints).Concat(newPathPoints).ToList();
+            var closestIndex = FindClosestPointIndex(concat.ToArray(), transform.position);
+            
+            var startIndex = Mathf.Max(0, closestIndex - renderPoints / 2);
+            var endIndex = Mathf.Min(concat.Count - 1, closestIndex + renderPoints / 2);
+            var positions = concat.GetRange(startIndex, endIndex - startIndex + 1).ToArray();           
+
+            lineRenderer.positionCount = positions.Length;
+            lineRenderer.SetPositions(positions);
+        }
+        
+        private int FindClosestPointIndex(Vector3[] points, Vector3 targetPoint)
+        {
+            float closestDistance = Mathf.Infinity;
+            int closestIndex = -1;
+
+            for (int i = 0; i < points.Length; i++)
+            {
+                float distance = Vector3.Distance(points[i], targetPoint);
+                if (distance < closestDistance)
+                {
+                    closestDistance = distance;
+                    closestIndex = i;
+                }
+            }
+
+            return closestIndex;
+        }
+        
+        private Vector3[] CalculateBezierCurveLine(LTBezierPath bezierPath, int numPoints)
+        {
+            var points = new List<Vector3>();
+            float stepSize = bezierPath.length / numPoints;
+            
+            for (int i = 0; i <= numPoints; i++)
+            {
+                float t = i * stepSize;
+                Vector3 point = bezierPath.point(t);
+                points.Add(point);
+            }
+
+            return points.ToArray();
         }
     }
 }
