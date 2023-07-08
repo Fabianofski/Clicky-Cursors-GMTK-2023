9c35b5f30331ca753598f96396cfc36cd66d9803
Fabian Friedrich <friedrich.fabian@gmx.net>
2023-07-08 13:25:48 +0200

    Added Better Cursor Paths

--- Assets/Scripts/Core/CursorMovement.cs
+++ Assets/Scripts/Core/CursorMovement.cs
@@ -18,6 +18,7 @@ namespace F4B1.Core
     {
         [SerializeField] private float speed;
         [SerializeField] private LineRenderer lineRenderer;
+        [SerializeField] private float pathLength;
         [SerializeField] private int accuracy;
         
         [SerializeField] private Vector2 bottomLeft;
@@ -98,79 +99,34 @@ namespace F4B1.Core
 
         private void UpdateLineRendererPath()
         {
-            // var oldPathPoints = CalculateBezierCurveLine(oldPath, Mathf.RoundToInt(oldPath.length) * accuracy);
-            var pathPoints = CalculateBezierCurveLine(path, Mathf.RoundToInt(path.length) * accuracy);
-            // var newPathPoints = CalculateBezierCurveLine(newPath, Mathf.RoundToInt(newPath.length) * accuracy);
+            var currentTime = activeAnimation.passed / activeAnimation.time;
+            var targetStart = (activeAnimation.passed - pathLength) / activeAnimation.time;
+            var targetEnd = (activeAnimation.passed + pathLength) / activeAnimation.time;
 
-            // var concat = oldPathPoints.Concat(pathPoints).Concat(newPathPoints).ToList();
-            //
-            // var index = Mathf.RoundToInt((activeAnimation.passed / activeAnimation.time) * pathPoints.Length) + oldPathPoints.Length;
+            var positions = new List<Vector3>();
 
-            // var truncated = concat.GetRange(index, concat.Count - index);
-            // var positions = GetLineWithDesiredLength(truncated, 3).ToArray();
-            // Debug.Log(truncated.Count + " " + concat.Count);
-            // concat.Reverse();
-            // var endLinePositions = GetLineWithDesiredLength(concat.GetRange(index, concat.Count - index), 3).ToArray();
-            // var positions = startLinePositions.Concat(endLinePositions).ToArray();
+            if (targetStart < 0)
+                positions = positions.Concat(CalculateBezierCurveLine(oldPath, 1 + targetStart, 1, accuracy)).ToList();            
+            positions = positions.Concat(CalculateBezierCurveLine(path, 
+                                    Mathf.Max(targetStart, 0), 
+                                    Mathf.Min(targetEnd, 1), 
+                                           accuracy)).ToList();
+            if (targetEnd > 1)
+                positions = positions.Concat(CalculateBezierCurveLine(newPath, 0, 1 - targetEnd, accuracy)).ToList();
 
-            lineRenderer.positionCount = pathPoints.Length;
-            lineRenderer.SetPositions(pathPoints);
-            //
-            // oldLineRenderer.positionCount = oldPathPoints.Length;
-            // oldLineRenderer.SetPositions(oldPathPoints);
-            //
-            // curLineRenderer.positionCount = pathPoints.Length;
-            // curLineRenderer.SetPositions(pathPoints);
-            //
-            // newLineRenderer.positionCount = newPathPoints.Length;
-            // newLineRenderer.SetPositions(newPathPoints);
+            lineRenderer.positionCount = positions.Count;
+            lineRenderer.SetPositions(positions.ToArray());
         }
         
-        private List<Vector3> GetLineWithDesiredLength(List<Vector3> points, float desiredLength)
-        {
-            var result = new List<Vector3>();
-
-            float totalLength = 0f;
-            for (int i = 1; i < points.Count; i++)
-            {
-                totalLength += Vector3.Distance(points[i - 1], points[i]);
-            }
-
-            float currentLength = 0f;
-            for (int i = 1; i < points.Count; i++)
-            {
-                float segmentLength = Vector3.Distance(points[i - 1], points[i]);
-                float remainingLength = desiredLength - currentLength;
-
-                if (remainingLength <= 0f)
-                    break;
-
-                if (currentLength + segmentLength <= desiredLength)
-                {
-                    result.Add(points[i]);
-                    if(segmentLength >= 0.1f)
-                        currentLength += segmentLength;
-                }
-                else
-                {
-                    float t = remainingLength / segmentLength;
-                    Vector3 interpolatedPoint = Vector3.Lerp(points[i - 1], points[i], t);
-                    result.Add(interpolatedPoint);
-                    currentLength += remainingLength;
-                }
-            }
-
-            return result;
-        }
-        
-        private Vector3[] CalculateBezierCurveLine(LTBezierPath bezierPath, int numPoints)
+        private Vector3[] CalculateBezierCurveLine(LTBezierPath bezierPath, float startTime, float endTime, int numPoints)
         {
             var points = new List<Vector3>();
             float stepSize = bezierPath.length / numPoints;
             
-            for (int i = 0; i <= numPoints; i++)
+            for (int i = Mathf.RoundToInt(startTime / stepSize); i <= numPoints; i++)
             {
                 float t = i * stepSize;
+                if (t > endTime) return points.ToArray();
                 Vector3 point = bezierPath.point(t);
                 points.Add(point);
             }
