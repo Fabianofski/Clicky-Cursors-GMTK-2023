362f5585e28c4df7f979ac2fa7235a800d756bd6
Fabian Friedrich <friedrich.fabian@gmx.net>
2023-07-08 12:50:02 +0200

    Added dynamic ShopManager and Shopbuttons

---
+++ Assets/Scripts/UI/ShopButton.cs
@@ -0,0 +1,30 @@
+﻿// /**
+//  * This file is part of: GMTK-2023
+//  * Created: 08.07.2023
+//  * Copyright (C) 2023 Fabian Friedrich
+//  * Distributed under the terms of the MIT license (cf. LICENSE.md file)
+//  **/
+
+using TMPro;
+using UnityAtoms.BaseAtoms;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace F4B1.UI
+{
+    public class ShopButton: MonoBehaviour
+    {
+
+        [SerializeField] private Button button;
+        [SerializeField] private TextMeshProUGUI titleTextField;
+        [SerializeField] private TextMeshProUGUI costTextField;
+        
+        public void SetButtonInformation(string title, int cost, VoidEvent clickEvent)
+        {
+            titleTextField.text = title;
+            costTextField.text = cost + "";
+
+            button.onClick.AddListener(clickEvent.Raise);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/ShopManager.cs
@@ -0,0 +1,45 @@
+﻿// /**
+//  * This file is part of: GMTK-2023
+//  * Created: 08.07.2023
+//  * Copyright (C) 2023 Fabian Friedrich
+//  * Distributed under the terms of the MIT license (cf. LICENSE.md file)
+//  **/
+
+using System;
+using UnityAtoms;
+using UnityAtoms.BaseAtoms;
+using UnityEngine;
+
+namespace F4B1.UI
+{
+    [Serializable]
+    public class ShopItem
+    {
+        public VoidEvent clickEvent;
+        public string title;
+        public int cost;
+    }
+    
+    public class ShopManager : MonoBehaviour
+    {
+
+        [SerializeField] private Transform buildingsTab;
+        [SerializeField] private Transform recipesTab;
+        [SerializeField] private Transform hardwareTab;
+
+        [SerializeField] private GameObject shopButtonPrefab;
+
+        [SerializeField] private ShopItem[] buildingItems;
+        [SerializeField] private ShopItem[] recipesItems;
+        [SerializeField] private ShopItem[] hardwareItems;
+
+        private void Awake()
+        {
+            foreach (var hardwareItem in hardwareItems)
+            {
+                GameObject go = Instantiate(shopButtonPrefab, hardwareTab);
+                go.GetComponent<ShopButton>().SetButtonInformation(hardwareItem.title, hardwareItem.cost, hardwareItem.clickEvent);
+            }
+        }
+    }
+}
