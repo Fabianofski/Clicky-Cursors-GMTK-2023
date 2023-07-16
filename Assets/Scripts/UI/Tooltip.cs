33c2c7f4e96970f45710b7cd4f9b7ab8658d28d5
Fabian Friedrich <68951860+Fabianofski@users.noreply.github.com>
2023-07-16 12:02:57 +0200

    Added new Tooltip indicator

---
+++ Assets/Scripts/UI/Tooltip.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace F4B1.UI
+{
+    public class Tooltip : MonoBehaviour
+    {
+        private void OnEnable()
+        {
+            var gameWasStarted = PlayerPrefs.HasKey("StartedBefore");
+            gameObject.SetActive(!gameWasStarted);
+            Debug.Log(gameWasStarted);
+            PlayerPrefs.SetInt("StartedBefore", 1);
+        }
+    }
+}
